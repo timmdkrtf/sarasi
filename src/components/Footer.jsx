bf907721df29100ea276c55f3dae64bc778c538c
timmdkrtf <contact@mdkrtf.co.id>
2025-03-12 10:24:25 +0700

    revisi footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,49 @@
-
+import { BsWhatsapp } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { MdLocalPhone } from "react-icons/md";
 
 export default function Footer(){
     return(
-        <div className="footer">
+        <footer id="kontak">
             <div className="container">
-                <div className="copyright">
-                    <p>© 2025 PT Abdi Sarana Asri &middot; All Rights Reserved</p>
+                <div className="row justify-content-between">
+                    <div className="col-lg-5 col-md-6 col-12">
+                        <h2>PT Abdi Sarana Asri</h2>
+                        <ul style={{listStyleType:"none", padding:"0"}}>
+                            <li className="pb-2">
+                                <a href="https://maps.app.goo.gl/8yRLm8ACmjuVgoQy8" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    Jl. Aria Surialaga No.83, Kota Bogor 16119
+                                </a>
+                            </li>
+                            <li className="pb-2">
+                                <a href="tel:+622518632170" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <MdLocalPhone className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    +62 251 8632 170
+                                </a>
+                            </li>
+                            <li className="pb-2">
+                                <a href="https://api.whatsapp.com/send?phone=6281211113526" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <BsWhatsapp className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    +62 812 1111 3526
+                                </a>
+                            </li>
+                            <li className="pb-2">
+                                <a href="mailto:contact@sarasi.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    <MdEmail className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    contact@sarasi.co.id
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="bottom">
+                    <hr />
+                    <p className="text-end mb-0 pb-3 Poppins" style={{fontSize:"12px"}}>© 2025 PT Abdi Sarana Asri &middot; All Rights Reserved</p>
                 </div>
             </div>
-        </div>
+        </footer>
     );
 }
