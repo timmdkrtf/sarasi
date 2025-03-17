8aaa06b83db24d7c04587f6f8a2225d99f3fea70
timmdkrtf <contact@mdkrtf.co.id>
2025-03-17 13:42:10 +0700

    revisi footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,27 +9,27 @@ export default function Footer(){
                 <div className="row justify-content-between">
                     <div className="col-lg-5 col-md-6 col-12">
                         <h2>PT Abdi Sarana Asri</h2>
-                        <ul style={{listStyleType:"none", padding:"0"}}>
+                        <ul style={{listStyleType:"none", padding:"0",marginTop: "-1px"}}>
                             <li className="">
-                                <a href="https://maps.app.goo.gl/8yRLm8ACmjuVgoQy8" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    Jl. Aria Surialaga No.83, Kota Bogor 16119
+                                <a href="https://maps.app.goo.gl/YTDTvrX94kKfCDvH6" target="_blank" style={{color: "white", textDecoration: "none"}}>
+                                    Jl. Aria Surialaga No. 83, Kota Bogor 16119
                                 </a>
                             </li>
                             <li className="">
                                 <a href="tel:+622518632170" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <MdLocalPhone className="fs-5" style={{ margin:"-3px 20px 0 0" }} />
+                                    <BsFillTelephoneFill className="fs-5" style={{ margin:"-2px 15px 0 0" }} />
                                     +62 251 8632 170
                                 </a>
                             </li>
                             <li className="">
                                 <a href="https://api.whatsapp.com/send?phone=6281211113526" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <BsWhatsapp className="fs-5" style={{ margin:"-3px 20px 0 0" }} />
+                                    <BsWhatsapp className="fs-5" style={{ margin:"-2px 15px 0 0" }} />
                                     +62 812 1111 3526
                                 </a>
                             </li>
                             <li className="">
                                 <a href="mailto:contact@sarasi.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <MdEmail className="fs-5" style={{ margin:"-3px 20px 0 0" }} />
+                                    <BsFillEnvelopeFill className="fs-5" style={{ margin:"-2px 15px 0 0" }} />
                                     contact@sarasi.co.id
                                 </a>
                             </li>
