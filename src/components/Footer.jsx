71bf3550dd548888dad23c63bfee0f68e452043a
timmdkrtf <contact@mdkrtf.co.id>
2025-03-17 11:07:32 +0700

    revisi footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,6 @@
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BsFillEnvelopeFill } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
-import { FaTiktok } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-import { MdLocalPhone } from "react-icons/md";
 
 export default function Footer(){
     return(
@@ -13,26 +10,26 @@ export default function Footer(){
                     <div className="col-lg-5 col-md-6 col-12">
                         <h2>PT Abdi Sarana Asri</h2>
                         <ul style={{listStyleType:"none", padding:"0"}}>
-                            <li className="pb-2">
+                            <li className="">
                                 <a href="https://maps.app.goo.gl/8yRLm8ACmjuVgoQy8" target="_blank" style={{color: "white", textDecoration: "none"}}>
                                     Jl. Aria Surialaga No.83, Kota Bogor 16119
                                 </a>
                             </li>
-                            <li className="pb-2">
+                            <li className="">
                                 <a href="tel:+622518632170" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <MdLocalPhone className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    <MdLocalPhone className="fs-5" style={{ margin:"-3px 20px 0 0" }} />
                                     +62 251 8632 170
                                 </a>
                             </li>
-                            <li className="pb-2">
+                            <li className="">
                                 <a href="https://api.whatsapp.com/send?phone=6281211113526" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <BsWhatsapp className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    <BsWhatsapp className="fs-5" style={{ margin:"-3px 20px 0 0" }} />
                                     +62 812 1111 3526
                                 </a>
                             </li>
-                            <li className="pb-2">
+                            <li className="">
                                 <a href="mailto:contact@sarasi.co.id" target="_blank" style={{color: "white", textDecoration: "none"}}>
-                                    <MdEmail className="me-3 fs-5" style={{ marginTop:"-3px" }} />
+                                    <MdEmail className="fs-5" style={{ margin:"-3px 20px 0 0" }} />
                                     contact@sarasi.co.id
                                 </a>
                             </li>
@@ -41,7 +38,7 @@ export default function Footer(){
                 </div>
                 <div className="bottom">
                     <hr />
-                    <p className="text-end mb-0 pb-3 Poppins" style={{fontSize:"12px"}}>© 2025 PT Abdi Sarana Asri &middot; All Rights Reserved</p>
+                    <p className="text-end mb-0">© 2025 PT Abdi Sarana Asri &middot; All Rights Reserved</p>
                 </div>
             </div>
         </footer>
